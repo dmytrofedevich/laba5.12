f7a56816ac0d186aeb187be3f20362f2f21aad1d
dmytrofedevich <105153672+dmytrofedevich@users.noreply.github.com>
2022-05-26 13:26:01 +0300

    Add files via upload

---
+++ one.java
@@ -0,0 +1,11 @@
+package Package;
+public class one {
+	public static void main(String[] args) {
+		int[]array=new int[98];
+		for(int i=0,a=1;a<99;i++,a+=2) {
+			array[i]=a;
+			System.out.print(array[i]+" ");
+		}
+		
+	}
+}
